bb75d9a3bdad14cb670403d2a08c539306713f29
Katrin <katrin.brueheim@gmx.de>
2024-04-13 15:15:30 +0200

    Removed hello world from index file.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { fetchFromEtsy } from './etsyApi';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;  // Portnummer definieren
+const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+// Route to handle Etsy API calls
+app.get('/etsy', fetchFromEtsy);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
